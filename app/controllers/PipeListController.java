8f6b278e8aa034a2ffa3f52ca00602c5ab9eb4da
Grassman <daniel.gronberg@diabol.se>
2012-08-29 13:14:30 +0200

    Updated routes with new names

--- app/controllers/PipeListController.java
+++ app/controllers/PipeListController.java
@@ -4,14 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import play.mvc.Controller;
 import play.mvc.Result;
 import utils.PipeConfReader;
-import views.html.allPipes;
+import views.html.pipelist;
 
 public class PipeListController extends Controller {
 
   private static PipeConfReader configReader = PipeConfReader.getInstance() ;
 
   public static Result index() {
-    return ok(allPipes.render(configReader.getConfiguredPipes()));
+    return ok(pipelist.render(configReader.getConfiguredPipes()));
   }
   
 }
